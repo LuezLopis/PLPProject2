dd2b72c97d897574ededaa301c28ed960e6b24b5
Luis T Lopez <ltalopez@outlook.com>
2025-12-04 02:19:27 -0600

    Implement maze wandering functionality
    Added wander function to navigate maze and read instructions.

--- Mazethingy.pl
+++ Mazethingy.pl
@@ -5,10 +5,38 @@ basic_map([[w,s,w],
 
 
 find_exit(Maze, Instructlst) :- 
-  first_row(Maze, Row1), find_start(s, Row1, 0)
+           first_row(Maze, Row1), 
+           find_start(s, Row1, 0),
+           % function to take us to start
+           wander(Maze, Instructlst, 1, Loc, s)
+  
+
 
 find_start(Start , [H|_], Loc). // ends after start is found
 find_start(Start , [_|T], Loc) :-
-  find_start(Start , T).
+           find_start(Start, T, Loc1), Loc is Loc1 + 1.
 
 first_row([[H|_]|_], H).
+
+maze_element(Matrix, R, C, Element) :-
+           nth0(R, Matrix, Row),   % Get the row
+           nth0(C, Row, Element).  % Get element from that row
+
+wander(Maze, Instructlst, CurrR, CurrC, Val) :-
+           read_instruct(Instructlst)
+           
+           
+read_instruct([H|T]) :-
+           Direct = H, 
+
+do_direct(left) :- 
+           C1 is C, C is C1 - 1.
+
+do_direct(right) :- 
+           C1 is C, C is C1 - 1.
+
+do_direct(up) :- 
+           C1 is C, C is C1 - 1.
+
+do_direct(down) :- 
+           R1 is R, C is C1 - 1.
