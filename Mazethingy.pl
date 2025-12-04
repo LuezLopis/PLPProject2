474c1d6af7af5180a7eac91658c0bf176ec534af
Luis T Lopez <ltalopez@outlook.com>
2025-12-03 21:55:12 -0600

    Add maze definition and find_exit predicate

--- Mazethingy.pl
+++ Mazethingy.pl
@@ -1,1 +1,10 @@
-find_exit(Maze, Instructlst).
+Maze(s, f, e).
+
+find_exit(Maze, Instructlst) :- 
+  first_row(Maze, Row1), find_start(s, Row1, 0)
+
+find_start(Start , [H|_], Loc). // ends after start is found
+find_start(Start , [_|T], Loc) :-
+  find_start(Start , T).
+
+first_row([[H|_]|_], H).
