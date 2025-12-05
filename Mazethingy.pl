020c03d4d360c6aafa2b9020f4f0f4e9a62ccde5
LuezLopis <ltalopez@outlook.com>
2025-12-05 16:03:33 -0600

    Updated wander function to include cut to allow for function exit when exit is found.

--- Mazethingy.pl
+++ Mazethingy.pl
@@ -5,38 +5,43 @@ basic_map([[w,s,w],
 
 
 find_exit(Maze, Instructlst) :- 
-           first_row(Maze, Row1), 
-           find_start(s, Row1, 0),
-           % function to take us to start
-           wander(Maze, Instructlst, 1, Loc, s)
+    first_row(Maze, Row1), 
+    find_start(s, Row1, 0),
+    % function to take us to start
+   wander(Maze, Instructlst, 1, Loc, s)
   
 
 
 find_start(Start , [H|_], Loc). // ends after start is found
 find_start(Start , [_|T], Loc) :-
-           find_start(Start, T, Loc1), Loc is Loc1 + 1.
+    find_start(Start, T, Loc1), Loc is Loc1 + 1.
 
 first_row([[H|_]|_], H).
 
 maze_element(Matrix, R, C, Element) :-
-           nth0(R, Matrix, Row),   % Get the row
-           nth0(C, Row, Element).  % Get element from that row
+    nth0(R, Matrix, Row),   % Get the row
+    nth0(C, Row, Element).  % Get element from that row
 
-wander(Maze, Instructlst, CurrR, CurrC, Val) :-
-           read_instruct(Instructlst)
-           
+wander(Maze, Instructlst, CurrR, CurrC) :-
+    maze_element(Maze, CurrR, CurrC, CurrVal), % takes out the current location  
+    CurrVal = e,
+    !,    % will exit if exit is found
+    read_instruct(Instructlst), % reads instruction
+    do_direct(Direct), // 
+    
            
 read_instruct([H|T]) :-
-           Direct = H, 
+    Direct = H; 
 
 do_direct(left) :- 
-           C1 is C, C is C1 - 1.
+    C1 is C, C is C1 - 1.
 
 do_direct(right) :- 
-           C1 is C, C is C1 - 1.
+    C1 is C, C is C1 + 1.
 
 do_direct(up) :- 
-           C1 is C, C is C1 - 1.
+   R1 is R, R is R1 - 1.
 
 do_direct(down) :- 
-           R1 is R, C is C1 - 1.
+   R1 is R, R is R1 + 1.
+
