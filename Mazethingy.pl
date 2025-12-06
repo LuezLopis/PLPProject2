585e4c7786894bfac6baf26a888115cdaa7abef7
LuezLopis <ltalopez@outlook.com>
2025-12-06 01:55:22 -0600

    FInished prototype of Mazethingy, with all functions fully fleshed out in concept.

--- Mazethingy.pl
+++ Mazethingy.pl
@@ -3,14 +3,17 @@ basic_map([[w,s,w],
            [f,f,w],
            [e,w,w]]).
 
+basic_instruct([down, left, down]).
+
 
 find_exit(Maze, Instructlst) :- 
     first_row(Maze, Row1), 
     find_start(s, Row1, 0),
     % function to take us to start
-   wander(Maze, Instructlst, 1, Loc, s)
-  
+   wander(Maze, Instructlst, 1, Loc, s).
 
+find_start(Start, [], Loc):-
+    write('There is no start '), nl.
 
 find_start(Start , [H|_], Loc). % ends after start is found
 find_start(Start , [_|T], Loc) :-
@@ -23,27 +26,33 @@ maze_element(Matrix, R, C, Element) :-
     nth0(C, Row, Element).  % Get element from that row
 
 wander(Maze, Instructlst, CurrR, CurrC, CurrVal) :-
-    (Spot = s; Spot = f), % current spot is valid and not the exit
-    next_spot_is(CurrVal, Instructlst, CurrR, CurrC), % checks the next location
+    (CurrVal = s; CurrVal = f), % current spot is valid and not the exit
+    next_spot_is( Instructlst, CurrR, CurrC). % checks the next location
     
-    
-    
-next_spot_is(Spot, Currlst, R1, C1) :- % if the spot is f(floor) or s(start) then read next instruct
+wander(Maze, Instructlst, CurrR, CurrC, CurrVal) :- 
+    CurrVal = e, % exit has been found has been found
+    write('Exit has been found '), nl.
+
+wander(Maze, [], CurrC, CurrR, CurrVal) :- 
+    write('Out of Instruction '), nl.    
+
+next_spot_is(Currlst, R1, C1) :- % if the spot is f(floor) or s(start) then read next instruct
     read_instruct(Currlst, Direct, Remainlst), % reads instruction
     do_direct(Direct), % manipulates the coordinate of the curr to the next instruction direction
-    maze_element(Maze, R1, C1, CurrVal), % takes out the next location 
-    
-is_wall(Spot) :-
-    Spot == w,
-    go_back();
+    maze_element(Maze, R1, C1, NextSpot), % takes out the next location 
+    is_wall(NextSpot).
 
 is_wall(Spot) :-
-    Spot \== w,
-    wander(Maze, );
+    Spot == w, % if there is a wall don't do instruct
+    wander(Maze, Remainlst, CurrR, CurrC, CurrVal).
+
+is_wall(Spot) :-
+    Spot \== w, % if not a wall then proceed to next spot
+    wander(Maze, Remainlst, R1, C1, NextSpot).
 
 read_instruct([H|T], Direct, Remainlst) :-
     Direct = H,
-    Remainlst = T; 
+    Remainlst = T. 
 
 do_direct(left) :- 
     C1 is C, C is C1 - 1.
@@ -56,5 +65,3 @@ do_direct(up) :-
 
 do_direct(down) :- 
    R1 is R, R is R1 + 1.
-
-go_back()
