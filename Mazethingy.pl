a22d2c18a30c3947042cd90a6e65ca32ac837912
LuezLopis <ltalopez@outlook.com>
2025-12-06 20:23:03 -0600

    Had a couple of typos in the start row finder

--- Mazethingy.pl
+++ Mazethingy.pl
@@ -2,7 +2,8 @@ big_map([[s,w,f,f],
          [f,w,f,w],
          [f,f,f,w],
          [w,f,e,w]]).
-big_instruct([down, down, right, right, down, left, down, left]).     
+big_instruct([down, down, right, right, down, left, down, left]).
+
 spiral_map([[s,f,f,f],
             [w,w,f,w],
             [f,f,f,w],
@@ -10,31 +11,43 @@ spiral_map([[s,f,f,f],
             [e,f,f,f]]).
 spiral_instruct([right, right, down, down, left, down, down, left]).
 
+middle_map([[e,f,f,f,f],
+            [w,w,f,w,w],
+            [f,f,s,w,w],
+            [w,f,w,w,w],
+            [e,f,f,f,w]]).
+middleA_instruct([left, down, down, left]).
+middleB_instruct([up, up, left, left]).
+
+
 find_exit(Maze, Instructlst) :- 
     %first_row(Maze, Row), 
-    find_start_in_maze(Maze, 0, LocR, LocC),
+    find_start_in_maze(Maze, LocR, LocC),
     % function to take us to start
    wander(Maze, Instructlst, LocR, LocC, s).
 
-find_start_in_maze([Row|_], CurrR, CurrC, Col):-
-    find_start_in_row(Row, 0, Col), %found in row
-    !. 
 
-find_start_in_maze([_|RemainMaze], CurrRow, SearchRow, SearchCol) :-
-    NextRow is CurrentRow + 1,
-    find_start_in_maze(RemainMaze, NextRow, SearchRow, SearchCol).
+find_start_in_maze([Row|_], 0, Col):-
+    find_start_in_row(Row, 0, Col), !. %found in row
 
-find_start_in_row([s|_], Loc, Loc) :- !. % s is found in the front of this row or whats left of the row
+find_start_in_maze([_|RemainMaze], CurrRow, Col) :-
+    find_start_in_maze(RemainMaze, PrevRow, Col),
+    CurrRow is PrevRow + 1.
 
-find_start_in_row([_|RemainRow], Loc, Col) :-
-    NextCol is Loc + 1,
+
+find_start_in_row([s|_], CurrLoc, CurrLoc) :- !. % s is found in the front of this row or whats left of the row
+find_start_in_row([_|RemainRow], CurrLoc, Col) :-
+    NextCol is CurrLoc + 1,
     find_start_in_row(RemainRow, NextCol, Col).
 
+
 %first_row([H|_], H). as S might not be in the first row
+
 
 maze_element(Matrix, R, C, Element) :-
     nth0(R, Matrix, Row),   % Get the row
     nth0(C, Row, Element).  % Get element from that row
+
 
 wander(Maze, Instructlst, CurrR, CurrC, e) :- % exit has been found has been found
     write('Exit has been found '), nl, !.
@@ -52,6 +65,7 @@ wander(Maze, [Direct|Remainlst], CurrR, CurrC, CurrVal) :-
     do_direct(Direct, CurrR, CurrC, NEWR, NEWC), % manipulates the coordinate of the curr to the next instruction direction
     maze_element(Maze, NEWR, NEWC, NextSpot), % takes out the next location 
     wander(Maze, Remainlst, NEWR, NEWC, NextSpot).
+
 
 do_direct(left, OGR, OGC, OGR, NEWC) :- 
     NEWC is OGC - 1,
